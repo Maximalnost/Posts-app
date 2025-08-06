cec174b617b16102e13b16a78fa0f8ed38eca46c
Maxim Vinokurov <ochenstrashno666@gmail.com>
2025-08-06 22:07:57 +0300

    check Submit disabled

---
+++ script.js
@@ -0,0 +1,54 @@
+const authForm = document.querySelector('#auth-form');
+const authFormEmail = document.querySelector('#auth-form-email');
+const authFormPassword = document.querySelector('#auth-form-password');
+const authFormSubmit = document.querySelector('#auth-form-submit');
+
+const validationRules = {
+    emailRegex: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    passwordRegex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{4,}$/,
+}
+
+const formValidation = {
+    email: false,
+    password: false,
+};
+
+const checkDisabled = () => {
+    if (formValidation.email && formValidation.password) {
+        authFormSubmit.disabled = false;
+    } else {
+        authFormSubmit.disabled = true;        
+    }
+}
+
+authForm.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    const formObj = new FormData(e.target);
+    const formData = Object.fromEntries(formObj);
+    if (
+        formData.email.match(validationRules.emailRegex) && 
+        formData.password.match(validationRules.passwordRegex)
+    ) {
+        console.log('yep');
+
+}})
+
+authFormEmail.addEventListener('input', (e) => {
+    if (e.target.value.match(validationRules.emailRegex)) {
+        formValidation.email = true;
+        authFormPassword.disabled = false;
+    } else {
+        formValidation.email = false;
+        authFormPassword.disabled = true;
+    }
+    checkDisabled();
+})
+
+authFormPassword.addEventListener('input', (e) => {
+    if (e.target.value.match(validationRules.passwordRegex)) {
+        formValidation.password = true;
+    } else {
+        formValidation.password = false;
+    }
+    checkDisabled();
+})
